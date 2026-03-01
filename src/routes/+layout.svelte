1927fd00729e4d01018953983dabbcca4358f133
Matus Barta <matus.barta64@gmail.com>
2026-03-01 01:00:54 +0000

    add dark mode switcher and gh link/stars

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,11 @@
 	import AppSidebar from '$lib/components/app-sidebar.svelte';
 	import { Separator } from '$lib/components/ui/separator/index.js';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import DarkModeSwitcher from '$lib/components/dark-mode-switcher.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import Github from '@lucide/svelte/icons/github';
 
-	let { data, children }: LayoutProps = $props();
+	let { data, children, params }: LayoutProps = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
@@ -17,14 +20,23 @@
 <Sidebar.Provider style="--sidebar-width: 19rem;">
 	<AppSidebar />
 	<Sidebar.Inset>
-		<header class="flex h-16 shrink-0 items-center gap-2 px-4">
-			<Sidebar.Trigger class="-ms-1" />
-			<Separator orientation="vertical" class="me-2 data-[orientation=vertical]:h-4" />
-			<span>
-				{data.hostname}
-				<span class="text-sm font-light italic">running on</span>
-				<span>{data.prettyName}</span>
-			</span>
+		<header class="flex h-16 shrink-0 items-center justify-between px-4">
+			<div class="flex shrink-0 items-center gap-2">
+				<Sidebar.Trigger class="-ms-1" />
+				<Separator orientation="vertical" class="me-2 data-[orientation=vertical]:h-4" />
+				<span>
+					{data.system.hostname}
+					<span class="text-sm font-light italic">running on</span>
+					<span>{data.system.prettyName}</span>
+				</span>
+			</div>
+			<div class="flex gap-2">
+				<Button href="https://github.com/matus-barta/fileserver" variant="ghost">
+					<Github />
+					<span class="truncate text-xs leading-tight text-muted-foreground">{data.ghStars}</span>
+				</Button>
+				<DarkModeSwitcher />
+			</div>
 		</header>
 		<div class="flex flex-1 flex-col gap-4 p-4">
 			{@render children()}
---
+++ src/routes/+layout.ts
@@ -0,0 +1,22 @@
+import type { LayoutLoad } from './$types';
+
+export const load: LayoutLoad = async ({ fetch, data }) => {
+	const ghUrl = 'https://api.github.com/repos/matus-barta/fileserver';
+	interface ghApi {
+		watchers: number;
+	}
+
+	const watchers = (
+		await fetch(ghUrl)
+			.catch((e) => {
+				console.error(e);
+				return e.message; //FIXME: handle fetch error
+			})
+			.then((res) => res.json() as Promise<ghApi>)
+	).watchers;
+
+	return {
+		ghStars: watchers,
+		system: data
+	};
+};
