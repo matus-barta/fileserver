0b5c6c4815ba1cdc10686a9a64bee0ae88d2d2a8
Matus Barta <matus.barta64@gmail.com>
2026-03-08 20:08:32 +0000

    wasted hours

--- src/lib/components/toolbar.svelte
+++ src/lib/components/toolbar.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-	let { children } = $props();
+	import { cn } from '$lib/utils.js';
+	let { children, class: className = '' } = $props();
 </script>
 
-<toolbar class="flex h-14 w-full flex-row items-center gap-3 rounded-xl border bg-sidebar px-2">
+<toolbar
+	class={cn('w-full items-center gap-3 rounded-xl border bg-sidebar px-2 py-2.25', className)}
+>
 	{@render children()}
 </toolbar>
--- src/routes/navigator/+page.svelte
+++ src/routes/navigator/+page.svelte
@@ -18,6 +18,14 @@
 	import DataTable from './data-table.svelte';
 	import type { Table as TanStackTable } from '@tanstack/table-core';
 
+	import { innerWidth } from 'svelte/reactivity/window';
+	import { cn } from '$lib/utils';
+
+	const mounts = [{ value: '/' }, { value: '/mnt/smb' }];
+
+	let mount = $state(mounts[0].value);
+	const triggerContent = $derived(mounts.find((f) => f.value === mount)?.value);
+
 	const nodes: Node[] = [
 		{
 			type: 'file',
@@ -35,35 +43,43 @@
 		}
 	];
 
+	// svelte-ignore non_reactive_update
 	let table: TanStackTable<Node> | undefined;
 </script>
 
-<Toolbar>
-	<ButtonGroup.Root>
+{#snippet navBtns()}
+	<div class="flew-row flex w-fit gap-3">
+		<ButtonGroup.Root>
+			<Button size="icon" variant="outline">
+				<ArrowLeft />
+			</Button>
+			<Button size="icon" variant="outline">
+				<ArrowRight />
+			</Button>
+		</ButtonGroup.Root>
 		<Button size="icon" variant="outline">
-			<ArrowLeft />
+			<CornerRightUp />
 		</Button>
 		<Button size="icon" variant="outline">
-			<ArrowRight />
+			<RefreshCw />
 		</Button>
-	</ButtonGroup.Root>
-	<Button size="icon" variant="outline">
-		<CornerRightUp />
-	</Button>
-	<Button size="icon" variant="outline">
-		<RefreshCw />
-	</Button>
-	<Button size="icon" variant="outline">
-		<Trash2 />
-	</Button>
-	<ButtonGroup.Root class="w-full">
+		<Button size="icon" variant="outline">
+			<Trash2 />
+		</Button>
+	</div>
+{/snippet}
+
+{#snippet path(cls: string)}
+	<ButtonGroup.Root class={cn(cls)}>
 		<Input value="akask" height={10} />
 		<Button size="icon" variant="outline">
 			<Copy />
 		</Button>
 	</ButtonGroup.Root>
+{/snippet}
 
-	<InputGroup.Root class="max-w-sm">
+{#snippet search(cls: string)}
+	<InputGroup.Root class={cn(cls)}>
 		<InputGroup.Input
 			placeholder="Search by name..."
 			value={(table?.getColumn('name')?.getFilterValue() as string) ?? ''}
@@ -78,15 +94,63 @@
 			<SearchIcon />
 		</InputGroup.Addon>
 	</InputGroup.Root>
+{/snippet}
 
-	<Select.Root type="single">
-		<Select.Trigger class="w-45"></Select.Trigger>
+{#snippet mountPoint(cls?: string)}
+	<Select.Root type="single" bind:value={mount}>
+		<Select.Trigger class={cn('min-w-40', cls)}>{triggerContent}</Select.Trigger>
 		<Select.Content>
-			<Select.Item value="light">Light</Select.Item>
-			<Select.Item value="dark">Dark</Select.Item>
-			<Select.Item value="system">System</Select.Item>
+			{#each mounts as mount (mount.value)}
+				<Select.Item value={mount.value} label={mount.value}>
+					{mount.value}
+				</Select.Item>
+			{/each}
 		</Select.Content>
 	</Select.Root>
-</Toolbar>
+{/snippet}
+
+<!-- i hate css svelte FTW -->
+{#if innerWidth.current != undefined}
+	{#if innerWidth.current > 1280}
+		<Toolbar class="flex flex-row">
+			{@render navBtns()}
+			{@render path('flex-2')}
+			{@render search('flex-1')}
+			{@render mountPoint()}
+		</Toolbar>
+	{:else if innerWidth.current > 874}
+		<Toolbar class="flex flex-col">
+			<div class="flex w-full flex-row gap-3">
+				{@render navBtns()}
+				{@render path('w-full')}
+			</div>
+
+			<div class="flex w-full flex-row gap-3">
+				{@render search('w-full')}
+				{@render mountPoint('w-full')}
+			</div>
+		</Toolbar>
+	{:else if innerWidth.current > 767}
+		<Toolbar class="flex flex-col items-start">
+			{@render navBtns()}
+			{@render path('w-full')}
+
+			{@render search('w-full')}
+			{@render mountPoint('w-full')}
+		</Toolbar>
+	{:else}
+		<Toolbar class="flex flex-col">
+			<div class="flex w-full flex-row gap-3">
+				{@render navBtns()}
+				{@render path('w-full')}
+			</div>
+
+			<div class="flex w-full flex-row gap-3">
+				{@render search('w-full')}
+				{@render mountPoint('w-full')}
+			</div>
+		</Toolbar>
+	{/if}
+{/if}
 
 <DataTable data={nodes} {columns} bind:table />
